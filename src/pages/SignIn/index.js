eb718944c92bc8f3910fe138495b2261a4eb39bd
Igor Souza <igorsouza.dev@gmail.com>
2019-10-09 21:42:27 -0300

    Added sign in and sign up pages

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
 import Background from '~/components/Background';
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 
-export default function SignIn() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
+
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+  function handleSubmit() {}
   return (
     <Background>
-      <Input icon="call" placeholder="Insert your name" />
-      <Button>Sign In</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Insert your e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            placeholder="Insert your password"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+          <SubmitButton onPress={handleSubmit}>SIGN IN</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignUp')}>
+          <SignLinkText>Create a free account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,11 +1,61 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef } from 'react';
+import { Image } from 'react-native';
 import Background from '~/components/Background';
 
-export default function SignUp() {
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+import logo from '~/assets/logo.png';
+
+export default function SignUp({ navigation }) {
+  const mailRef = useRef();
+  const passwordRef = useRef();
+
+  function handleSubmit() {}
+
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            placeholder="Insert your full name"
+            returnKeyType="next"
+            onSubmitEditing={() => mailRef.current.focus()}
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            placeholder="Insert your e-mail"
+            ref={mailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Insert your password"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+          />
+          <SubmitButton onPress={handleSubmit}>SIGN UP</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Already have an account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
