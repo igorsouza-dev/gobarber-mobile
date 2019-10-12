e8a30118d7f99620cb9d4d931de608632e450e84
Igor Souza <igorsouza.dev@gmail.com>
2019-10-12 05:52:47 -0300

    Adding pages navigation. Adding dashboard styles

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Background from '~/components/Background';
+import { Container, Title } from './styles';
 
 export default function Dashboard() {
-  return <View />;
+  return (
+    <Background>
+      <Container>
+        <Title>Appointments</Title>
+      </Container>
+    </Background>
+  );
 }
+
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Appointments',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" color={tintColor} size={20} />
+  ),
+};
